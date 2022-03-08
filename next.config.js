bd80ade5ec089de4a395246664c7f25c0552b1f3
EvernoteMC <evernoteminecraft@gmail.com>
2022-03-08 13:13:55 +0530

    feat: add privacy and terms (wip)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+	async redirects() {
+		return [
+			{
+				source: '/donate',
+				destination: 'https://github.com/sponsors/ObserverMC',
+				permanent: true,
+			},
+			{
+				source: '/invite',
+				destination:
+					'https://discord.com/oauth2/authorize?client_id=813718772908163082&permissions=388168&scope=applications.commands%20bot',
+				permanent: true,
+			},
+			{
+				source: '/support',
+				destination: 'https://discord.com/invite/YyCyZtchg3',
+				permanent: true,
+			},
+		];
+	},
 	reactStrictMode: true,
 };
