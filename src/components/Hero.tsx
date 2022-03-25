b7f8c45b410186aaba16389bc47a608726cfcd4a
Matt <46137770+xMdb@users.noreply.github.com>
2022-03-25 15:42:18 +1000

    fix(mobile): fix hero on mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,29 +3,41 @@ import NextLink from 'next/link';
 
 export const Hero = () => {
 	return (
-		<Box height='90vh'>
-			<Flex justifyContent="center" alignItems="center" flexDirection="column" p={30} pt={150} textAlign="center">
+		<Box height="90vh">
+			<Flex
+				justifyContent="center"
+				alignItems="center"
+				flexDirection="column"
+				p={30}
+				pt={[100, 150, 200]}
+				textAlign="center">
 				<Text fontSize="6xl" fontWeight="900" pb={5} lineHeight="120%" mb={25}>
 					Hypixel Statistics
 					<br />
 					<span className="textGradient">Redefined</span>
 				</Text>
-				<Text fontSize="xl" fontWeight="regular" pb={5} wordBreak="break-word" maxWidth="50ch" mb={75}>
+				<Text
+					fontSize={['lg', 'xl']}
+					fontWeight="regular"
+					pb={5}
+					wordBreak="break-word"
+					maxWidth="50ch"
+					mb={75}>
 					A unique Hypixel stats bot that allows users to view their daily and weekly BedWars, SkyWars, and
 					Duels statistics.
 				</Text>
-				<Box>
-					<NextLink href="/invite">
-						<Button variant="main" px="40px" mx={10}>
+				<Flex flexDirection="row">
+					<NextLink href="/invite" passHref>
+						<Button variant="main" px="40px" mx={5}>
 							Invite Me
 						</Button>
 					</NextLink>
-					<NextLink href="/about">
-						<Button variant="secondary" px="40px" mx={10}>
+					<NextLink href="/about" passHref>
+						<Button variant="secondary" px="35px" mx={5}>
 							Learn More
 						</Button>
 					</NextLink>
-				</Box>
+				</Flex>
 			</Flex>
 		</Box>
 	);
