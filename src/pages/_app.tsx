5f457214d32deab9564c93cd49d150cb2497be39
Matt <46137770+xMdb@users.noreply.github.com>
2022-04-03 18:19:19 +1000

    fix(fonts): add missing font weights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@fontsource/montserrat/900.css';
 import '@fontsource/montserrat/800.css';
 import '@fontsource/montserrat/700.css';
+import '@fontsource/montserrat/600.css';
+import '@fontsource/montserrat/500.css';
 import '@fontsource/montserrat/400.css';
 import Script from 'next/script';
 
